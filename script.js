1d7b70399b7c45386b6a92e1b1cd8e232d3dc66e
Mark B DSouza <mark.benjamin.dsouza@gmail.com>
2020-06-05 00:01:37 +0530

    JS update

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
 const navLinks = document.querySelectorAll('.navigation__link');
 const navToggle = document.getElementById('navi-toggle');
-
-console.log(navLinks);
-
 function hideNav() {
-  console.log(navToggle);
   navToggle.checked = false;
 }
-
 navLinks.forEach((el) => {
   el.addEventListener('click', hideNav);
 });
